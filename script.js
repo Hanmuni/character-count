cdfe119ac5d81bf5e8b4a71fc434be5524f8a0f7
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2023-11-08 22:24:55 +0100

    feat: remove if statement

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let blabla = document.querySelector("#text");
+let textarea = document.querySelector("#text");
 let amount = document.querySelector("#amount");
 
-blabla.addEventListener("input", () => {
-  if (blabla.value) {
-    amount.textContent = text.value.length;
-  }
+textarea.addEventListener("input", () => {
+  amount.textContent = textarea.value.length;
 });
