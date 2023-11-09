1b45bde01fd77cc8fbc40dd34954c10e6a992c51
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2023-11-09 20:03:41 +0100

    feat: empty branch

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-let textarea = document.querySelector("#text");
-let amount = document.querySelector("#amount");
 
-textarea.addEventListener("input", () => {
-  amount.textContent = textarea.value.length;
-});
