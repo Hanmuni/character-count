890ad23d473f1b38fabbbf43a29efb785b55beab
Hồng-Hạnh, Chu <67581292+Hanmuni@users.noreply.github.com>
2023-11-19 21:18:06 +0100

    feat: refactor code

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ textarea.addEventListener("input", () => {
   let words = text.value.split(/[\s]+/g).filter((word) => {
     return word.length;
   });
-  count.textContent = `You've written ${words.length} words and ${text.value.length} characters.`;
+  count.textContent = `You've written ${words.length} words and ${textarea.value.length} characters.`;
 });
